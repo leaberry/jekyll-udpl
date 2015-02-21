4e3d7b1e2578534d54b0a8a6b2dc0a8ad6863fa3
Daniel Leaberry <dleaberry@iarchives.com>
2015-02-21 12:17:27 -0700

    Exclude rsync'ing the h5ai cache directory

--- build-and-push-site.sh
+++ build-and-push-site.sh
@@ -27,5 +27,5 @@ xattr -rc _site
 
 ## Set permissions and push
 chmod -R 755 _site
-rsync -au --delete -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
+rsync -auv --delete --exclude _h5ai/cache/ -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
 
