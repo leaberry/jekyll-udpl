41070816badf465f2664991b70ab644202ab614b
Daniel Leaberry <dleaberry@iarchives.com>
2015-03-03 16:12:21 -0700

    Don't overwrite the lists directory in preparation for production hosting with phplist

--- build-and-push-site.sh
+++ build-and-push-site.sh
@@ -27,5 +27,5 @@ xattr -rc _site
 
 ## Set permissions and push
 chmod -R 755 _site
-rsync -auv --delete --exclude _h5ai/cache/ -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
+rsync -auv --delete --exclude _h5ai/cache/ --exclude lists/ -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
 
--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 xattr -rc _site
 chmod -R 755 _site
-rsync -auv --delete --exclude _h5ai/cache/ -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
+rsync -auv --delete --exclude _h5ai/cache/ --exclude lists/ -e ssh _site/ udplmail@192.168.244.30:/var/www/beta_udpl
